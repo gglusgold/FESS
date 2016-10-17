14c6cea2e82f83828d2b1ffbd2c206946bda248a
tano-racing <racinglocura07gmail.com>
2016-10-17 16:13:28 -0300

    Falta hacer parte de los graficos

--- Economia Social Y Solidaria/Controllers/EstadisticasController.cs
+++ Economia Social Y Solidaria/Controllers/EstadisticasController.cs
@@ -9,9 +9,18 @@ namespace Economia_Social_Y_Solidaria.Controllers
 {
     public class Estadistica
     {
+        public int ComunaNumero;
         public string Comuna;
         public int Compras;
-        //public int Cuando;
+
+    }
+
+    public class ProductosVecino
+    {
+        public string Nombre;
+        public string Vecino;
+        public int Cantidad;
+
     }
 
 
@@ -23,22 +32,36 @@ namespace Economia_Social_Y_Solidaria.Controllers
             TanoNEEntities ctx = new TanoNEEntities();
             List<Estadistica> lista = new List<Estadistica>();
 
-
-            EstadosCompra Entregado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo ==3);
-
-            lista = ctx.Compras.GroupBy(a => a.Locales.comuna).Select(b => new Estadistica { Comuna = "Comuna " + b.Key, Compras = b.Count() }).ToList();
-
-            //var ls = ctx.Compras.Where(a => a.EstadosCompra.codigo > Entregado.codigo).GroupBy(a => a.Locales.comuna).Select(a =>
-            //{
-
-            //});
-            /*lista = ctx.Compras.Where(a => a.EstadosCompra.codigo > Entregado.codigo).Select(com => new Estadistica
-            {
-                Comuna = "Comuna " + com.Locales.comuna,
-                Compras = com.
-            }).ToList();*/
+            EstadosCompra Entregado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo == 3);
+            lista = ctx.Compras.Where(a => a.EstadosCompra.codigo >= Entregado.codigo).GroupBy(a => a.Locales.comuna).Select(b => new Estadistica { ComunaNumero = b.Key, Comuna = "Comuna " + b.Key, Compras = b.Count() }).ToList();
 
             return View(lista);
+        }
+
+        public JsonResult ProductoPorComuna(int comuna)
+        {
+            TanoNEEntities ctx = new TanoNEEntities();
+
+            EstadosCompra Entregado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo == 3);
+            var listado = ctx.CompraProducto.Where(a => a.Compras.EstadosCompra.codigo >= Entregado.codigo && a.Compras.Locales.comuna == comuna)
+                .GroupBy(a => a.Productos.nombre)
+                .Select(b => new { 
+                    Nombre = b.Key,
+                    Cantidad = b.Sum(a => a.cantidad)
+                });
+
+
+            var quienes = ctx.CompraProducto.Where(a => a.Compras.EstadosCompra.codigo >= Entregado.codigo && a.Compras.Locales.comuna == comuna)
+                .GroupBy(x => new { x.Productos.nombre, x.Compras.Vecinos.nombres })
+                .Select(b => new
+                {
+                    Producto = b.Key.nombre,
+                    Vecino = b.Key.nombres,
+                    Cantidad = b.Sum(a => a.cantidad)
+                });
+
+
+            return Json(new { listado = listado, quienes = quienes });
         }
 
     }
