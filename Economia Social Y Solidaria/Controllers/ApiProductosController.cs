e8e4ab2228d22e171173c5326212167db960bb9c
Tano <Tano@DESKTOP-1Q9JUGV>
2017-02-12 22:03:40 -0300

    usuario

--- Economia Social Y Solidaria/Controllers/ApiProductosController.cs
+++ Economia Social Y Solidaria/Controllers/ApiProductosController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Economia_Social_Y_Solidaria.Models;
+using System.Web;
 
 namespace Economia_Social_Y_Solidaria.Controllers
 {
@@ -70,5 +71,79 @@ namespace Economia_Social_Y_Solidaria.Controllers
 
             //return Json(locales);
         }
+
+        [ActionName("Usuario")]
+        [HttpPost]
+        public IHttpActionResult Usuario() // string correo, string pass)
+        {
+            TanoNEEntities ctx = new TanoNEEntities();
+
+            var form = HttpContext.Current.Request.Form;
+
+            string correo = form["correo"];
+            string pass = form["pass"];
+
+            Vecinos vecino = ctx.Vecinos.FirstOrDefault(a => a.correo == correo);
+            if (vecino != null)
+            {
+
+                if (!vecino.verificado)
+                {
+                    return Json(new { Error = "Hay que verificar la cuenta. Revisá tu mail" });
+                }
+
+                pass = InicioController.GetCrypt(pass);
+                if (vecino.contrasena == pass)
+                {
+                    EstadosCompra EstadoEntregado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo == 1);
+                    EstadosCompra confirmado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo == 3);
+                    EstadosCompra comentado = ctx.EstadosCompra.FirstOrDefault(a => a.codigo == 4);
+
+                    //Precios ultimop = prod.Precios.Count > 1 ? prod.Precios.LastOrDefault(a => a.fecha.Date <= actual.fechaAbierto.Date) : prod.Precios.FirstOrDefault();
+
+
+                    var json = ctx.Compras.Where(a => a.vecinoId == vecino.idVecino).OrderByDescending(a => a.fecha).ToList().Select(a => new
+                    {
+                        idCompra = a.idCompra,
+                        estado = a.EstadosCompra.nombre,
+                        fecha = a.fecha.ToString("hh:mm dd/MM/yyyy"),
+                        idLocal = a.Locales.idLocal,
+                        local = a.Locales.direccion,
+                        barrio = a.Locales.barrio,
+                        editar = a.estadoId == EstadoEntregado.idEstadoCompra,
+                        comentar = a.estadoId == confirmado.idEstadoCompra,
+                        comentado = a.estadoId == comentado.idEstadoCompra,
+                        comuna = a.Locales.comuna,
+                        productos = a.CompraProducto.ToList().Select(b => new
+                        {
+                            idProducto = b.Productos.idProducto,
+                            nombre = b.Productos.producto,
+                            marca = b.Productos.marca,
+                            presentacion = b.Productos.presentacion,
+                            cantidad = b.cantidad,
+                            comentado = a.ComentariosProducto.FirstOrDefault(c => c.productoId == b.productoId) != null,  // a.Comentarios.Count == 1 ? a.Comentarios.FirstOrDefault().ComentariosProducto.FirstOrDefault(cp => cp.productoId == b.productoId).Productos != null : false,
+                            precioUnidad = b.Productos.Precios.Count > 1 ? b.Productos.Precios.LastOrDefault(precio => a.fecha.Date >= precio.fecha.Date).precio : b.Productos.Precios.FirstOrDefault().precio,
+
+                        })
+                    }).ToList();
+
+                    var jsonvecino = new
+                    {
+                        idVecino = vecino.idVecino,
+                        nombre = vecino.nombres,
+                        telefono = vecino.telefono,
+                        comuna = vecino.comuna
+                    };
+
+                    return Json(new { Historico = json, Vecino = jsonvecino });
+                }
+                else
+                    return Json(new { Error = "Contraseña incorrecta" });
+            }
+            else
+                return Json(new { Error = "No existe el usuario, para usar la plataforma es necesario registrarse " });
+
+        }
+
     }
 }
